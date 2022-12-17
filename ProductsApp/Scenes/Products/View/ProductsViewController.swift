6bff7ed3155ef050a7b1bf400d07562efcd1cdc6
Hussein <hussein.elsherief@gmail.com>
2022-12-17 11:59:52 +0200

    - Initial commit

---
+++ ProductsApp/Scenes/Products/View/ProductsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProductsViewController.swift
+//  ProductsApp
+//
+//  Created by Hussein Anwar on 17/12/2022.
+//
+
+import UIKit
+
+class ProductsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
